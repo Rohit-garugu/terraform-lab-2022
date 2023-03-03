e5a8bf7b32190fa8264a023731ea2968dbbec84d
Rohit-garugu <82309941+Rohit-garugu@users.noreply.github.com>
2023-03-03 04:54:09 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0f8ca728008ff5af4"
+  ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
-  availability_zone = "ap-south-1"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "HelloWorld-dev"
   }
